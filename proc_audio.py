e04b24a33ae25760fc5327280fcaca89dd273ce1
mcomunita <m.comunita@qmul.ac.uk>
2022-11-03 11:07:28 +0000

    proc_audio with cpu

--- proc_audio.py
+++ proc_audio.py
@@ -1,10 +1,10 @@
+import argparse
+import torch
+
+from scipy.io.wavfile import write
+
 import src.utils as utils
 import src.data as data
-import src.gcntfilm as gcntfilm
-import argparse
-from scipy.io.wavfile import write
-import torch
-import time
 
 
 def parse_args():
@@ -12,9 +12,9 @@ def parse_args():
         description='''This script takes an input .wav file, loads it and processes it with a neural network model of a
                     device, i.e guitar amp/pedal, and saves the output as a .wav file''')
 
-    parser.add_argument('--input_file', type=str, help='input file (e.g., "data/test/facebender-input.wav")')
-    parser.add_argument('--output_file', type=str, help='output file (e.g., "facebender-output.wav")')
-    parser.add_argument('--model_file', type=str, help='model file (e.g., "results/MODEL_FOLDER/model_best.json")')
+    parser.add_argument('--input_file', type=str, default='data/test/ht1-input.wav', help='input file')
+    parser.add_argument('--output_file', type=str, default='output.wav', help='output file')
+    parser.add_argument('--model_file', type=str, default='results/ht1-ht11/model_best.json', help='model file')
     parser.add_argument('--chunk_length', type=int, default=16384, help='chunk length')
     return parser.parse_args()
 
@@ -29,17 +29,22 @@ def proc_audio(args):
 
     # cuda
     if torch.cuda.is_available():
+        device = torch.device("cuda:0")
         torch.set_default_tensor_type("torch.cuda.FloatTensor")
-        torch.cuda.set_device(0)
+        # torch.cuda.set_device(0)
         print("\ncuda device available")
-        model = model.cuda()
+    else:
+        device = torch.device("cpu")
+        torch.set_default_tensor_type("torch.FloatTensor")
+        print("\ncuda device NOT available")
 
+    model = model.to(device)
     model.eval()
 
     with torch.no_grad():
         input_data = dataset.subsets['data'].data['data'][0]
 
-        input_data = input_data.cuda()
+        input_data = input_data.to(device)
         # output = network(input_data)
 
         _, _, output = model.process_data(input_data,
